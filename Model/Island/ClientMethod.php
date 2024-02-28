cefb809506353f3288f07845402425d5d5acb65d
ubermanu <manu@paca.happy-dev.fr>
2024-02-28 12:38:57 +0100

    feat: add client method

---
+++ Model/Island/ClientMethod.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ubermanu\Motu\Model\Island;
+
+class ClientMethod
+{
+    public const METHOD_NONE = null;
+    public const METHOD_LOAD = 'load';
+    public const METHOD_IDLE = 'idle';
+    public const METHOD_MEDIA = 'media';
+    public const METHOD_VISIBLE = 'visible';
+}
